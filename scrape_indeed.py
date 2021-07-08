a6d6bca9d5dc536d353b0ba11c08eae13db0749c
Michael Mll <michaelmuell95@gmail.com>
2021-07-08 16:31:35 +0200

    2. step completed: scrape multiple pages based on user

--- scrape_indeed.py
+++ scrape_indeed.py
@@ -5,51 +5,60 @@ import pandas as pd
 import time
 import html5lib
 
-indeed_html = requests.get('https://www.indeed.com/jobs?q=SQL&l=Remote&sort=date&vjk=1d8515cad94915e9').text
-soup = BeautifulSoup(indeed_html, 'lxml')
+def getpage(indeed_html):
+    soup = BeautifulSoup(indeed_html, 'lxml')
 
-jobs_list = soup.find('div', id = 'mosaic-provider-jobcards')
-jobs = jobs_list.find_all('a', class_='tapItem', href=True)
+    jobs_list = soup.find('div', id = 'mosaic-provider-jobcards')
+    jobs = jobs_list.find_all('a', class_='tapItem', href=True)
 
-i = 0
+    i = 0
 
-for job in jobs:
-    job_title = job.find('span', class_=lambda x: x != 'label').text
-    print(job_title)
+    for job in jobs:
+        job_title = job.find('span', class_=lambda x: x != 'label').text
+        print(job_title)
 
-    company_location = job.find('div', class_='companyLocation').text
-    print(company_location)
+        company_location = job.find('div', class_='companyLocation').text
+        print(company_location)
 
-    company_name = job.find('span', class_='companyName').text
-    print(company_name)
+        company_name = job.find('span', class_='companyName').text
+        print(company_name)
 
-    if job.find('span', class_='ratingNumber')is not None:
-        company_rating = job.find('span', class_='ratingNumber').text
-        print(company_rating)
+        if job.find('span', class_='ratingNumber')is not None:
+            company_rating = job.find('span', class_='ratingNumber').text
+            print(company_rating)
 
-    if job.find('div', class_='job-snippet')is not None:
-        job_snippet = job.find('div', class_='job-snippet').text
-        print(job_snippet)
+        if job.find('div', class_='job-snippet')is not None:
+            job_snippet = job.find('div', class_='job-snippet').text
+            print(job_snippet)
 
-    if job.find('span', class_='date')is not None:
-        posted = job.find('span', class_='date').text
-        print(posted)
+        if job.find('span', class_='date')is not None:
+            posted = job.find('span', class_='date').text
+            print(posted)
 
-    if job.find('span', class_='salary-snippet')is not None:
-        salary_snippet = job.find('span', class_='salary-snippet').text
-        print(salary_snippet)
+        if job.find('span', class_='salary-snippet')is not None:
+            salary_snippet = job.find('span', class_='salary-snippet').text
+            print(salary_snippet)
 
-    link = job['href']
-    print(f'https://www.indeed.com'+ link)
+        link = job['href']
+        print(f'https://www.indeed.com'+ link)
 
-    print('____________________________________')
-    i = i+1
+        print('____________________________________')
+        i+=1
 
-print(i)
+    print(i)
+    return i
 
+input = 3
+counter = 0
+total_scrape_count = 0
 
+while counter < input:
+    indeed_html = requests.get(f'https://www.indeed.com/jobs?q=SQL&l=Remote&sort=date&start='+str(counter)+'&vjk=b7fcb744c2617103').text
+    counter+=1
+    result = getpage(indeed_html)
+    total_scrape_count = total_scrape_count + result
 
-
+print(total_scrape_count)
 
 #job_title_new = job.find('h2', class_ = 'jobTitle jobTitle-color-purple jobTitle-newJob')
 #job_title_old = job.find('h2', class_ = 'jobTitle jobTitle-color-purple jobTitle')
