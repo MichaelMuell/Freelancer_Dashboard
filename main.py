565703f8f87d9a79c97ba206e5a80671ecd59c32
Michael Muell <michaelmuell95@gmail.com>
2023-01-25 17:14:53 +0100

    Umbau OO

--- main.py
+++ main.py
@@ -4,46 +4,55 @@ import scrape_gulp
 import openpyxl as pxl
 import scrape_freelance
 
-queries = []
+class crawler():
+    def __init__(self,queries):
+        self.queries = queries
 
-query1 = ['gif','Data Analytics','Remote','date','contract', 1]
-query2 = ['gif','SQL','Remote','date','contract',1]
-query3 = ['gif','Power BI', 'Remote', 'date','contract',1]
-query4 = ['gif','Business Intelligence', 'Remote', 'date','contract',1]
-query5 = ['gif','DWH BI', 'Remote', 'date','contract',1]
-query6 = ['gif','Analytics Engineer', 'Remote', 'date','contract',1]
-query7 = ['gif','Data Warehouse', 'Remote', 'date','contract',1]
+    def start(self):
+        data_indeed = pd.DataFrame()
+        data_gulp = pd.DataFrame()
+        data_freelance = pd.DataFrame()
 
-queries.append(query1)
-queries.append(query2)
-queries.append(query3)
-queries.append(query4)
-queries.append(query5)
+        for query in self.queries.values():
+        
+            print(query)
+            data_freelance = freelance.getdata(query)
 
-data_indeed = pd.DataFrame()
-data_gulp = pd.DataFrame()
-data_freelance = pd.DataFrame()
+            platforms,key_words,location,sort,job_type,pages = query
 
-for query in queries:
+            if 'g' in platforms:
+                data_gulp = data_gulp.append(scrape_gulp.get_data(key_words,sort,pages))
 
-    platforms,key_words,location,sort,job_type,pages = query
+            if 'i' in platforms:
+                data_indeed = data_indeed.append(scrape_indeed.get_data(key_words,location,sort,job_type,pages))
 
-    if 'g' in platforms:
-        data_gulp = data_gulp.append(scrape_gulp.get_data(key_words,sort,pages))
+            if 'f' in platforms:
+                data_freelance = data_freelance.append(scrape_freelance.get_data(key_words,location,sort,pages))
 
-    if 'i' in platforms:
-        data_indeed = data_indeed.append(scrape_indeed.get_data(key_words,location,sort,job_type,pages))
+            print(data_gulp)
+            print(data_indeed)
+            print(data_freelance)
 
-    if 'f' in platforms:
-        data_freelance = data_freelance.append(scrape_freelance.get_data(key_words,location,sort,pages))
+        frames = [data_gulp,data_indeed,data_freelance]
+        output = pd.concat(frames)
 
-    print(data_gulp)
-    print(data_indeed)
-    print(data_freelance)
+        path = r'G:/My Drive/freelancer_jobs.xlsx'
 
-frames = [data_gulp,data_indeed,data_freelance]
-output = pd.concat(frames)
+        output.to_excel(path)
 
-path = r'G:/My Drive/freelancer_jobs.xlsx'
 
-output.to_excel(path)
+queries = {
+           1:['f','Data Analytics','Remote','date','contract', 1],
+           2:['f','SQL','Remote','date','contract',1],
+           3:['f','Power BI', 'Remote', 'date','contract',1],
+           4:['f','Business Intelligence', 'Remote', 'date','contract',1],
+           5:['f','DWH BI', 'Remote', 'date','contract',1],
+           6:['f','Analytics Engineer', 'Remote', 'date','contract',1],
+           7:['f','Data Warehouse', 'Remote', 'date','contract',1]
+        }
+
+print(queries)
+
+test = crawler(queries)
+
+test.start()
